9b605d6cdc99aa20889a3712cc2364152ae90445
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2020-08-01 22:59:35 +0200

    Add TooltipButton

---
+++ components/TooltipButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-tooltip bottom :open-delay="openDelay">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        icon
+        v-bind="attrs"
+        :disabled="disabled"
+        v-on="on"
+        @click="$emit('click')"
+      >
+        <slot></slot>
+      </v-btn>
+    </template>
+    <span>{{ $i18n.t(tooltip) }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  name: 'TooltipButton',
+  props: {
+    tooltip: {
+      type: String,
+      default: null,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    openDelay: {
+      type: Number,
+      default: 500,
+    },
+  },
+};
+</script>
